1223dab77bd753fa3af361f47a71b71c4c07440c
S.Mahammad <140738442+saddaladinnemahammad@users.noreply.github.com>
2025-08-07 10:59:43 +0530

    Add files via upload

---
+++ booking.js
@@ -0,0 +1,164 @@
+//all single single buttons works
+const sc1=document.getElementsByClassName('.subcontainer');
+const row1=document.getElementsByClassName('.r1');
+const buttons=document.querySelectorAll(".btns")
+const bt1=document.getElementById('b1');
+const bt2=document.getElementById('b2');
+const bt3=document.getElementById('b3');
+const bt4=document.getElementById('b4'); 
+const bt5=document.getElementById('b5');
+const bt6=document.getElementById('b6');
+const bt7=document.getElementById('b7');
+const bt8=document.getElementById('b8');
+const bt9=document.getElementById('b9');
+const bt10=document.getElementById('b10');
+bt1.addEventListener('click',function(){
+    let warning=alert('continue your booking');
+    let user=prompt("enter booking user name");
+    if(user){
+        console.log("confirmed");
+        bt1.style.backgroundColor="blue";
+    }
+    else{
+        console.log("no confirmed");
+        bt1.style.backgroundColor="red";
+    }
+    console.log(user);
+});
+bt2.addEventListener('click',function(){
+    let warning=alert('continue your booking');
+    let user=prompt("enter booking user name");
+    if(user){
+        console.log("confirmed");
+        bt2.style.backgroundColor="blue";
+    }
+    else{
+        console.log("no confirmed");
+        bt2.style.backgroundColor="red";
+    }
+    console.log(user);
+});
+bt2.addEventListener('click',function(){
+    let warning=alert('continue your booking');
+    let user=prompt("enter booking user name");
+    if(user){
+        console.log("confirmed");
+        bt2.style.backgroundColor="blue";
+    }
+    else{
+        console.log("no confirmed");
+        bt2.style.backgroundColor="red";
+    }
+    console.log(user);
+});
+bt3.addEventListener('click',function(){
+    let warning=alert('continue your booking');
+    let user=prompt("enter booking user name");
+    if(user){
+        console.log("confirmed");
+        bt3.style.backgroundColor="blue";
+    }
+    else{
+        console.log("no confirmed");
+        bt3.style.backgroundColor="red";
+    }
+    console.log(user);
+});
+bt4.addEventListener('click',function(){
+    let warning=alert('continue your booking');
+    let user=prompt("enter booking user name");
+    if(user){
+        console.log("confirmed");
+        bt4.style.backgroundColor="blue";
+    }
+    else{
+        console.log("no confirmed");
+        bt4.style.backgroundColor="red";
+    }
+    console.log(user);
+});
+bt5.addEventListener('click',function(){
+    let warning=alert('continue your booking');
+    let user=prompt("enter booking user name");
+    if(user){
+        console.log("confirmed");
+        bt5.style.backgroundColor="blue";
+    }
+    else{
+        console.log("no confirmed");
+        bt5.style.backgroundColor="red";
+    }
+    console.log(user);
+});
+bt6.addEventListener('click',function(){
+    let warning=alert('continue your booking');
+    let user=prompt("enter booking user name");
+    if(user){
+        console.log("confirmed");
+        bt6.style.backgroundColor="blue";
+    }
+    else{
+        console.log("no confirmed");
+        bt6.style.backgroundColor="red";
+    }
+    console.log(user);
+});
+bt7.addEventListener('click',function(){
+    let warning=alert('continue your booking');
+    let user=prompt("enter booking user name");
+    if(user){
+        console.log("confirmed");
+        bt7.style.backgroundColor="blue";
+    }
+    else{
+        console.log("no confirmed");
+        bt7.style.backgroundColor="red";
+    }
+    console.log(user);
+});
+bt8.addEventListener('click',function(){
+    let warning=alert('continue your booking');
+    let user=prompt("enter booking user name");
+    if(user){
+        console.log("confirmed");
+        bt8.style.backgroundColor="blue";
+    }
+    else{
+        console.log("no confirmed");
+        bt8.style.backgroundColor="red";
+    }
+    console.log(user);
+});
+bt9.addEventListener('click',function(){
+    let warning=alert('continue your booking');
+    let user=prompt("enter booking user name");
+    if(user){
+        console.log("confirmed");
+        bt9.style.backgroundColor="blue";
+    }
+    else{
+        console.log("no confirmed");
+        bt9.style.backgroundColor="red";
+    }
+    console.log(user);
+});
+bt10.addEventListener('click',function(){
+    let warning=alert('continue your booking');
+    let user=prompt("enter booking user name");
+    if(user){
+        console.log("confirmed");
+        bt10.style.backgroundColor="blue";
+    }
+    else{
+        console.log("no confirmed");
+        bt10.style.backgroundColor="red";
+    }
+    console.log(user);
+});
+
+
+
+
+
+
+
